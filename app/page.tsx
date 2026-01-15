b93745794d2dfbfe4738ee446aa536ce6604411e
Shreya Kumari <shreesim4226@gmail.com>
2026-01-15 20:34:25 +0530

    try to deploy 12

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
 import {IEvent} from "@/database";
-import {cacheLife} from "next/cache";
+
 
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
-    'use cache';
-    cacheLife('hours')
-    const response = await fetch(`${BASE_URL}/api/events`);
+ 
+   const response = await fetch(`${BASE_URL}/api/events`, {
+    next: { revalidate: 3600 } // revalidate every 1 hour
+  });
     const { events } = await response.json();
 
     return (
